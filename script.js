2bbd5648056526942a6e4505ea0c746efa74080a
gustavenoksson <gustav.enoksson@live.se>
2022-11-17 11:33:10 +0100

    added new logo

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const h5 = document.querySelector("h5");
 
 const minWidth = window.matchMedia("(min-width: 710px)");
 
+// if (window.minWidth="710px") {
+//   h5.textContent="Subscribe to our newsletter";
+// } 
+// else h5.textContent="Subscribe to our <br>newsletter";
+
 window.addEventListener("resize", () => {
   if (minWidth.matches) {
     h5.textContent="Subscribe to our newsletter";
-  }
+  } else h5.innerHTML=`Subscribe to our <br> newsletter`;
 });
