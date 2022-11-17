9b2652afd1cfbff0d086cf854aca00918e08f36b
gustavenoksson <gustav.enoksson@live.se>
2022-11-17 11:09:57 +0100

    added javascript

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const navIcon = document.querySelector("nav-icon img");
+const h5 = document.querySelector("h5");
 
-const minWidth = window.matchMedia("(min-width: 560px)");
+const minWidth = window.matchMedia("(min-width: 710px)");
 
 window.addEventListener("resize", () => {
   if (minWidth.matches) {
+    h5.textContent="Subscribe to our newsletter";
   }
 });
