a208e4bab012e3ee7c10d7ed54dedde9b4d39b60
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-11-17 09:49:00 +0100

    fixed responsive nav

---
+++ script.js
@@ -0,0 +1,8 @@
+const navIcon = document.querySelector("nav-icon img");
+
+const minWidth = window.matchMedia("(min-width: 560px)");
+
+window.addEventListener("resize", () => {
+  if (minWidth.matches) {
+  }
+});
